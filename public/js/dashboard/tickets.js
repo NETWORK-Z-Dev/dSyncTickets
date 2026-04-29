9cb4f7dfd6911525f8a97967f0a9fc046636ea39
Marcel <40896559+hackthedev@users.noreply.github.com>
2026-04-29 23:27:54 +0200

    Added ticket logic and styling

--- public/js/dashboard/tickets.js
+++ public/js/dashboard/tickets.js
@@ -41,16 +41,58 @@ async function displayTicketsInList(){
                 id: 123456789012,
                 first_name: "Max",
                 last_name: "Musterman",
-                company: {
-
-                }
+                icon: null,
+                company: {}
             },
             created: new Date().getTime(),
             status: "unassigned",
+            unread: false,
+        },
+        {
+            id: 67890,
+            title: "Server startet nicht mehr",
+            creator: {
+                id: 123456789013,
+                first_name: "Oompa",
+                last_name: "Loompa",
+                icon: null,
+                company: {}
+            },
+            created: new Date().getTime(),
+            status: "unassigned",
+            unread: true,
         }
     ]
 
     for(let ticket of tickets){
+        let creator = ticket.creator;
+
+
+        let ticketElement = document.createElement("div");
+        ticketElement.classList.add("ticket");
+
+        // if unread mark it as such
+        if(ticket.unread === true) ticketElement.classList.add("unread");
+
+        ticketElement.setAttribute("data-id", ticket.id);
+        ticketElement.innerHTML =
+            `
+            <div class="icon">${creator.icon ? "" : `${creator.first_name[0]}${creator.last_name[0]}`}</div>
+            <div class="content">
+                <p class="meta">
+                    ${creator.first_name} ${creator.last_name} <span class="timestamp">${new Date(ticket.created).toLocaleTimeString()}</span>
+                </p>
+                
+                <p>${ticket.title}</p>
+                
+                <p class="footer">
+                    <span class="status">${ticket.status}</span>
+                </p>
+            </div>
+            `;
+
+
         console.log(ticket)
+        getTicketListElement().appendChild(ticketElement);
     }
 }
